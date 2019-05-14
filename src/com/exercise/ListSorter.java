3cd00d012744f085d2fba87377abbf5bcd41ea59
heshibo <18800109197@163.com>
2019-05-14 22:42:45 +0800

    List sort Exercise

--- src/com/exercise/ListSorter.java
+++ src/com/exercise/ListSorter.java
@@ -3,6 +3,7 @@ package com.exercise;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /*List sort Exercise*/
 public class ListSorter<T> {
@@ -23,8 +24,8 @@ public class ListSorter<T> {
         }
     }
 
-    public void mergeSortInPlace(List<T> list,Comparator<T> comparator){
-        List<T> result = mergeSort(list,comparator);
+    public void mergeSortInPlace(List<T> list, Comparator<T> comparator) {
+        List<T> result = mergeSort(list, comparator);
         list.clear();
         list.addAll(result);
     }
@@ -43,26 +44,53 @@ public class ListSorter<T> {
         List<T> result = new LinkedList<>();
         int total = first.size() + second.size();
         for (int i = 0; i < total; i++) {
-            List<T> winner = pickWinner(first,second,comparator);
+            List<T> winner = pickWinner(first, second, comparator);
             result.add(winner.remove(0));
         }
         return result;
     }
 
-    private List<T> pickWinner(List<T> first, List<T> second, Comparator<T> comparator){
-        if(first.size() == 0){
+    private List<T> pickWinner(List<T> first, List<T> second, Comparator<T> comparator) {
+        if (first.size() == 0) {
             return second;
         }
-        if(second.size() == 0){
+        if (second.size() == 0) {
             return first;
         }
-        int cmp = comparator.compare(first.get(0),second.get(0));
-        if(cmp < 0){
+        int cmp = comparator.compare(first.get(0), second.get(0));
+        if (cmp < 0) {
             return first;
         }
-        if(cmp > 0){
+        if (cmp > 0) {
             return second;
         }
         return first;
+    }
+
+    /*4 digits*/
+    public List<T> radixSort(List<T> list) {
+        int position, num;
+        Queue<T>[] digitQueues = (LinkedList<T>[]) (new LinkedList[10]);
+        for (int digitVal = 0; digitVal < 10; digitVal++) {
+            digitQueues[digitVal] = (Queue<T>) new LinkedList<>();
+        }
+
+        //sort the list
+        for (int digit = 0; digit <= 3; digit++) {
+            for (int i = 0; i < list.size(); i++) {
+                position = Character.digit(String.valueOf(list.get(i)).charAt(3 - digit), 10);
+                digitQueues[position].add(list.get(i));
+            }
+
+            //gather numbers back into list
+            num = 0;
+            for (int digitVal = 0; digitVal < 10; digitVal++) {
+                while (!digitQueues[digitVal].isEmpty()) {
+                    list.set(num, digitQueues[digitVal].remove());
+                    num++;
+                }
+            }
+        }
+        return list;
     }
 }
